4c149448dad6cdda30bc4bc65469a8c38ed5e820
Ian Mobley <imobley@taskrabbit.com>
2021-10-12 09:32:16 -0700

    Set up async-graphql
    Moved greeting to query

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -1,17 +1,45 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Result};
+use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
+use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema};
+use async_graphql_actix_web::{Request, Response};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+type AppSchema = Schema<Query, EmptyMutation, EmptySubscription>;
+
+struct Query;
+
+#[Object]
+impl Query {
+    async fn get_greeting(&self) -> String {
+        "Hello World!".to_owned()
+    }
+}
+
+fn configure_service(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/")
+            .route(web::post().to(index))
+            .route(web::get().to(index_playground)),
+    );
+}
+
+async fn index(schema: web::Data<AppSchema>, req: Request) -> Response {
+    schema.execute(req.into_inner()).await.into()
+}
+
+async fn index_playground() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(playground_source(
+            GraphQLPlaygroundConfig::new("/").subscription_endpoint("/"),
+        )))
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()>{
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+async fn main() -> std::io::Result<()> {
+    let schema: AppSchema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
+
+    HttpServer::new(move || App::new().configure(configure_service).data(schema.clone()))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
