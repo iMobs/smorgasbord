401e3e7c15da4ca899fff889dd46e1b70cd7831d
Ian Mobley <imobley@taskrabbit.com>
2021-10-14 10:15:30 -0700

    Set up basic federation dummy example

---
+++ posts-service/src/posts.resolver.ts
@@ -0,0 +1,59 @@
+import {
+  Args,
+  Directive,
+  Field,
+  ID,
+  Int,
+  ObjectType,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
+
+@ObjectType()
+@Directive('@extends')
+@Directive('@key(fields: "id")')
+export class User {
+  @Field((type) => Int)
+  @Directive('@external')
+  id: number;
+
+  // @Field((type) => [Post])
+  // posts?: Post[];
+}
+
+@ObjectType()
+@Directive('@key(fields: "id")')
+export class Post {
+  @Field(() => ID)
+  id: number;
+  @Field()
+  title: string;
+  @Field()
+  content: string;
+
+  @Field((type) => Int)
+  authorId: number;
+
+  @Field((type) => User)
+  author?: User;
+}
+
+@Resolver((of) => Post)
+export class PostsResolver {
+  @Query(() => Post)
+  getPost(@Args('id') id: number): Post {
+    return {
+      id: 1,
+      title: 'Test Post',
+      content: 'This is a test',
+      authorId: 1,
+    };
+  }
+
+  @ResolveField((of) => User)
+  author(@Parent() post: Post): any {
+    return { __typename: 'User', id: post.authorId };
+  }
+}
