286f4af1063b197dbc5d4dc3e7456b76d04553ea
Ian Mobley <ianmobley@gmail.com>
2021-11-20 11:10:58 -0800

    Dockerize auth service

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -18,7 +18,7 @@ async fn main() -> std::io::Result<()> {
             .configure(configure_service)
             .app_data(Data::new(schema.clone()))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
